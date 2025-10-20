a1a7eca2ad3bf15b657a6a4a4aeb1050095428fa
darias-montesino-web <darias-montesino@sva.edu>
2025-10-19 22:29:29 -0400

    Added Player Animations
    Reworked walking and running speeds. Added an attack----currently does not work as intended. Need to code a way to check an animation has ended.

--- Game_Files/horror-game/Scripts/PlayerMovement.gd
+++ Game_Files/horror-game/Scripts/PlayerMovement.gd
@@ -1,15 +1,15 @@
 extends CharacterBody2D
 
 
-@export var walk_speed = 150.0
-@export var run_speed = 250
+@export var walk_speed = 250.0
+@export var run_speed = 400
 const JUMP_VELOCITY = -400.0
 
 @export_range(0,1) var deceleration = 0.1
 @export_range(0,1) var acceleration = 0.1
 
 var speed
-
+var run_type
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
@@ -17,24 +17,41 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
+	
+	# Handle jump -- not working idk why
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		animated_sprite.play("Jump")
+		
 	if Input.is_action_pressed("run"):
 		speed = run_speed
+		
 	else:
 		speed = walk_speed
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
-	if direction:
+	
+	# Handle running
+	if direction and not Input.is_action_pressed("jump") and Input.is_action_pressed("run") == true:
 		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
 		animated_sprite.play("Run")
 		animated_sprite.flip_h = direction < 0
+	
+	# Handle walking
 	else:
+		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
+		animated_sprite.play("Walk")
+		animated_sprite.flip_h = direction < 0
+	
+	# Handle standing
+	if !direction:
 		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
+		animated_sprite.flip_h = direction > 0
 		animated_sprite.play("Idle")
-
-
+		
+		
+	if Input.is_action_pressed("attack") and velocity.x == 0:
+		animated_sprite.play("Slash")
+		
 	move_and_slide()
